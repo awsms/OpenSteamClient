8639e7b70dd9cfdf33a9dc80f6669a9a2d45809c
awsms <48278661+awsms@users.noreply.github.com>
2026-08-31 02:05:37 +0200

    Capture library search shortcut at window level

--- OpenSteamClient/Views/Pages/LibraryPage.axaml.cs
+++ OpenSteamClient/Views/Pages/LibraryPage.axaml.cs
@@ -2,18 +2,39 @@ using System;
 using System.Collections.Generic;
 using OpenSteamClient.Extensions;
 using Avalonia.Controls;
+using Avalonia;
 using Avalonia.Markup.Xaml;
 using Avalonia.Input;
+using Avalonia.Interactivity;
 using OpenSteamClient.Controls;
 
 namespace OpenSteamClient.Views;
 
 public partial class LibraryPage : BasePage
 {
+    private TopLevel? keyBindingTopLevel;
+
     public LibraryPage() : base()
     {
         InitializeComponent();
         this.TranslatableInit();
+    }
+
+    protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e)
+    {
+        base.OnAttachedToVisualTree(e);
+        keyBindingTopLevel = TopLevel.GetTopLevel(this);
+        keyBindingTopLevel?.AddHandler(
+            InputElement.KeyDownEvent,
+            LibraryPage_OnKeyDown,
+            RoutingStrategies.Tunnel);
+    }
+
+    protected override void OnDetachedFromVisualTree(VisualTreeAttachmentEventArgs e)
+    {
+        keyBindingTopLevel?.RemoveHandler(InputElement.KeyDownEvent, LibraryPage_OnKeyDown);
+        keyBindingTopLevel = null;
+        base.OnDetachedFromVisualTree(e);
     }
 
     private void LibraryPage_OnKeyDown(object? sender, KeyEventArgs e)
